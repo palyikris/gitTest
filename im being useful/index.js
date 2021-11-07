a5c3622ab826de235a2746de3e3ea99f9c4e11bd
kris <palyi.kristof@gmail.com>
2021-11-07 17:33:58 +0100

    usefulness step 1.1

--- im being useful/index.js
+++ im being useful/index.js
@@ -1,3 +1,4 @@
+
 function toolsVisible(){
     document.getElementById('toolsReadyWrapper').style.display="flex";
     document.getElementById('toolsReady').style.display="flex";
@@ -20,4 +21,72 @@ function toolsnotReady(){
     document.getElementById('toolsReady').style.display='none';
     document.getElementById('toolsReadyOther').style.display='none';
     document.getElementById('inputWrapper').style.display='flex';
+}
+function darkMode(){
+    console.log(document.body.style.backgroundColor)
+    console.log(document.body.style.backgroundColor === "rgb(46, 44, 44)")
+    if(document.body.style.backgroundColor === "rgb(46, 44, 44)"){
+        document.body.style.backgroundColor="rgb(248, 244, 244)";
+        document.getElementById("main").style.backgroundColor="white";
+        const darkModeVars = document.querySelectorAll("button, label");
+        console.log(darkModeVars);
+        for(let i = 0; i<darkModeVars.length; i++){
+            console.log(darkModeVars[i].className === "addMainButton")
+            if(darkModeVars[i].className === "addMainButton"){
+                console.log(i)
+                darkModeVars[i].style.color="#02c2f2";
+            }
+            else{
+                darkModeVars[i].style.color="white";
+            }
+        }
+        document.getElementById(main).style.boxShadow="1px 1px lightgrey;";
+    }
+    else{
+        document.body.style.backgroundColor="#2e2c2c";
+        document.getElementById("main").style.backgroundColor="#544f4f";
+        const darkModeVars = document.querySelectorAll("button, label");
+        console.log(darkModeVars);
+        for(let i = 0; i<darkModeVars.length; i++){
+            if(darkModeVars[i].className === "addMainButton"){
+                console.log(i)
+                darkModeVars[i].style.color="#02c2f2";
+            }
+            else{
+                darkModeVars[i].style.color="#3d4d4d";
+            }
+        }
+        document.getElementById(main).style.boxShadow="none";
+    }
+}
+setInterval(() => {
+    let nOfChars = 0;
+    let nOfWords = 0;
+    let input = document.getElementById("main").value;
+    for(let i = 0; i < input.length; i++){
+        if(input[i] === ""){
+
+        }
+        else{
+            nOfChars++;
+        }
+    }
+    console.log(nOfChars)
+    input = input.split(" ");
+    console.log(input);
+    for(let i = 0; i < input.length; i++){
+        nOfWords++;
+    }
+    nOfWords = nOfWords-1;
+    document.getElementById("nOfWords").innerHTML="Szavak száma: "+nOfWords+", és a karakterek száma: "+nOfChars+".";
+}, 1000);
+function countWords(){
+    document.getElementById("inputWrapper").style.display="none";
+    document.getElementById("toolsReadyWrapper").style.display="none";
+    document.getElementById("nOfWords").style.display="block";
+}
+function noCountWords(){
+    document.getElementById("inputWrapper").style.display="flex";
+    document.getElementById("toolsReadyWrapper").style.display="none";
+    document.getElementById("nOfWords").style.display="none";
 }
